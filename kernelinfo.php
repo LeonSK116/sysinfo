06f81b5a5cd1ceaf582cfdffe4ab3cc57ac65fd7
leonsk116 <97976204+LeonSK116@users.noreply.github.com>
2023-05-23 23:30:45 +0700

    Update kernelinfo.php

--- kernelinfo.php
+++ kernelinfo.php
@@ -1,4 +1,7 @@
 <?php
+// Get the output of the previous shell script
+$output = shell_exec('cat /etc/yum.repos.d/*.repo | grep -B8 -e "enabled = 1" -e "enabled=1" | grep name | awk -F= \'{print $2}\'');
+
 // Get the output of hostname, uname and lsb_release commands
 $hostname = shell_exec('hostname');
 $kernel = shell_exec('uname -r');
@@ -14,6 +17,8 @@ echo "<h1>Server Info</h1>\n";
 echo "<p>The current hostname is $hostname.</p>\n";
 echo "<p>The current kernel version is $kernel.</p>\n";
 echo "<p>The current Linux distribution is $distro.</p>\n";
+echo "<h2>Repository Names</h2>\n";
+echo "<pre>$output</pre>";
 echo "</body>\n";
 echo "</html>\n";
 ?>
